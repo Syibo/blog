a995de627c6cc0a85e8d50c3d3e31e910a8118b7
shenyibo <1335638583@qq.com>
2020-08-24 14:51:43 +0800

    theme

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,11 +2,23 @@ module.exports = {
   base: '/blog/',
   title: 'syb',
   description: 'Vuepress blog demo',
+  theme: 'reco',
   themeConfig: {
     // 你的GitHub仓库，请正确填写
+    type: 'blog',
     repo: 'https://github.com/Syibo',
     // 自定义仓库链接文字。
     repoLabel: 'My GitHub',
+    blogConfig: {
+      category: {
+        location: 2,     // 在导航栏菜单中所占的位置，默认2
+        text: 'Category' // 默认文案 “分类”
+      },
+      tag: {
+        location: 3,     // 在导航栏菜单中所占的位置，默认3
+        text: 'Tag'      // 默认文案 “标签”
+      }
+    },
     nav: [
       { text: '首页', link: '/' },
       { text: '前端', link: '/front/FirstBlog.md' }
--- docs/front/FirstBlog.md
+++ docs/front/FirstBlog.md
@@ -1,1 +1,8 @@
-# FirstBlog
+---
+title: 【vue】跨域解决方案之proxyTable
+date: 2017-12-28
+categories:
+ - frontEnd
+tags:
+ - vue
+---
